db9a95c75bd58c3ea414f41ecac661f3ae92d4ed
choilc <choilv99@gmail.com>
2022-10-30 17:12:35 +0700

    add favicon and iamges

--- src/assets/images/wedding/index.js
+++ src/assets/images/wedding/index.js
@@ -7,6 +7,17 @@ import Im6 from "./6.jpg";
 import Im7 from "./7.jpg";
 import Im8 from "./8.jpg";
 import Im9 from "./9.jpg";
+import Im10 from "./10.jpg";
+import Im11 from "./11.jpg";
+import Im12 from "./12.jpg";
+import Im13 from "./13.jpg";
+import Im14 from "./14.jpg";
+import Im15 from "./15.jpg";
+import Im16 from "./16.jpg";
+import Im17 from "./17.jpg";
+import Im18 from "./18.jpg";
+import Im19 from "./19.jpg";
+import Im20 from "./20.jpg";
 
 export const Images = [
   {
@@ -48,5 +59,53 @@ export const Images = [
   {
     src: Im9,
     alt: "img9",
+  },
+  {
+    src: Im10,
+    alt: "img10",
+  },
+  {
+    src: Im11,
+    alt: "img11",
+  },
+  {
+    src: Im12,
+    alt: "img12",
+  },
+  {
+    src: Im13,
+    alt: "img13",
+  },
+  {
+    src: Im14,
+    alt: "img14",
+  },
+  {
+    src: Im15,
+    alt: "img15",
+  },
+  {
+    src: Im16,
+    alt: "img16",
+  },
+  {
+    src: Im17,
+    alt: "img17",
+  },
+  {
+    src: Im17,
+    alt: "img17",
+  },
+  {
+    src: Im18,
+    alt: "img18",
+  },
+  {
+    src: Im19,
+    alt: "img19",
+  },
+  {
+    src: Im20,
+    alt: "img20",
   },
 ];
